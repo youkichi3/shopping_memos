86934bf781cafcb31b8d7bc954b4c57ad40d28cf
youkichi3 <tatata_abcd@icloud.com>
2020-08-15 09:48:40 +0900

    share画面のfieldの幅を100%に

--- resources/js/components/Share.js
+++ resources/js/components/Share.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
 import { Button, TextField } from '@material-ui/core';
 
 class Share extends Component {
@@ -10,7 +9,7 @@ class Share extends Component {
                     <div className="col-12 text-center">
                         <h2 className="mt-5 mb-5">共有する</h2>
                         <span>相手の個人コードを入力してください。</span>
-                        <TextField className="mt-5" />
+                        <TextField className="mt-5 field" />
                         <Button className="text-center mt-5 pt-3 pb-3  mx-auto memo" variant="contained" color="primary">共有する</Button>
                     </div>
                 </div>
